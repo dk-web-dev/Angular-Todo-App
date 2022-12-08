fb6be3aa1b55b28e71e061230fa47823f9bcbb69
dharmesh-innvonix <dharmesh.kanetiya@innvonix.com>
2022-12-08 16:55:17 +0530

    add not found functionality and update folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { MaterialComponent } from './component/material/material.component';
 import { HomeComponent } from './component/home/home.component';
-import { TodosComponent } from './component/todos/todos.component';
+import { TodosComponent } from './component/TodoApp/todos/todos.component';
 import { ObservableComponent } from './component/observable/observable.component';
 import { TodoMainComponent } from './component/CRUD/todo-main/todo-main.component';
 import { EditComponent } from './component/CRUD/edit/edit.component';
+import { NotfoundComponent } from './component/notfound/notfound.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'material', component: MaterialComponent },
   { path: 'todos', component: TodosComponent },
-  {path:'observable',component:ObservableComponent},
-  {path:'crud',component:TodoMainComponent},
+  { path: 'observable', component: ObservableComponent },
+  { path: 'crud', component: TodoMainComponent },
   { path: 'edit/:id', component: EditComponent },
+  { path: '**', component: NotfoundComponent },
 ];
 
 @NgModule({
